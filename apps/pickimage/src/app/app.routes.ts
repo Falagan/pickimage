411d50c53dbe562f34be44571962dc6bf987a13d
Eloy Pérez <ep.falagan@gmail.com>
2025-05-31 00:10:26 +0200

    feat: setup main app shell layout and lazy route to search image feature

--- apps/pickimage/src/app/app.routes.ts
+++ apps/pickimage/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Route } from '@angular/router';
+import { IMAGE_REPOSITORY } from '@pickimage/domain';
+import { UnsplashRepository } from '@pickimage/repositories';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    providers: [
+      {
+        provide: IMAGE_REPOSITORY,
+        useClass: UnsplashRepository,
+      },
+    ],
+    loadComponent: () =>
+      import('@pickimage/features').then((c) => c.SearchImagesComponent),
+  },
+];
