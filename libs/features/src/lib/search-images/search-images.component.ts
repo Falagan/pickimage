bc2a3180ac901636e4b07f26ec35ad7c4b68273f
Eloy Pérez <ep.falagan@gmail.com>
2025-05-31 02:39:01 +0200

    feat: search image feature composition

--- libs/features/src/lib/search-images/search-images.component.ts
+++ libs/features/src/lib/search-images/search-images.component.ts
@@ -1,20 +1,30 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { InputSearchComponent } from '@pickimage/shared';
+import { ImageGalleryComponent } from '@pickimage/shared';
+import { Component, inject } from '@angular/core';
 import { ImagesService } from './services/images.service';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { ImageCardComponent } from './components/image-card/image-card.component';
 
 @Component({
-  selector: 'lib-search-images',
+  selector: 'feature-search-images',
   templateUrl: './search-images.component.html',
   styleUrl: './search-images.component.css',
-  imports: [AsyncPipe, NgFor, NgIf],
+  imports: [
+    AsyncPipe,
+    NgFor,
+    NgIf,
+    ImageCardComponent,
+    ImageGalleryComponent,
+    InputSearchComponent,
+  ],
   providers: [ImagesService],
   standalone: true,
 })
-export class SearchImagesComponent implements OnInit {
+export class SearchImagesComponent {
   private readonly imagesService = inject(ImagesService);
   public images$ = this.imagesService.images$;
 
-  ngOnInit(): void {
-    this.imagesService.getImages();
+  onSearch(text: string) {
+    this.imagesService.searchImages({ page: 1, itemsPerPage: 10, text });
   }
 }
