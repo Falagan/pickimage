8f23be7d6b2872ef33a9a41f1f883434b88b18cf
Eloy Pérez <ep.falagan@gmail.com>
2025-06-02 18:21:36 +0200

    feat: re define image detail with new subcomponents

--- libs/features/src/lib/image-detail/image-detail.component.ts
+++ libs/features/src/lib/image-detail/image-detail.component.ts
@@ -4,26 +4,41 @@ import {
   inject,
   OnInit,
 } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ImageDetailService } from './services/image-detail.service';
-import { AsyncPipe, NgIf } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
+import { ImageInfoComponent } from './components/image-info/image-info.component';
+import { ImageTagsComponent } from './components/image-tags/image-tags.component';
+import { ImageContentComponent } from './components/image-content/image-content.component';
+import { ImageGoToSearchComponent } from "./components/image-go-to-search/image-go-to-search.component";
 
 @Component({
   selector: 'feature-image-detail',
   templateUrl: './image-detail.component.html',
   styleUrl: './image-detail.component.scss',
-  imports: [NgIf, AsyncPipe],
-  providers: [ImageDetailService],
+  imports: [
+    AsyncPipe,
+    ImageInfoComponent,
+    ImageTagsComponent,
+    ImageContentComponent,
+    ImageGoToSearchComponent
+],
+  providers: [ImageDetailService, ImageInfoComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImageDetailComponent implements OnInit {
   private readonly routeService = inject(ActivatedRoute);
+  private readonly routerService = inject(Router);
   private readonly imageDetailService = inject(ImageDetailService);
   public readonly image$ = this.imageDetailService.image$;
 
   ngOnInit(): void {
     const id = this.getImageId();
     this.imageDetailService.getImage(id);
+  }
+
+  public onGoImageSearch() {
+    this.routerService.navigate(['/']);
   }
 
   private getImageId() {
